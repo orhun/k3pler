bd07b75bc1fbeabbd044720ac401dbbef34c7e89
KeyLo99 <keylo99official@gmail.com>
2018-09-12 15:25:41 +0200

    Dialog layout changed to right one. Warnings suppressed

--- app/src/main/java/com/tht/k3pler/RequestDialog.java
+++ app/src/main/java/com/tht/k3pler/RequestDialog.java
@@ -21,12 +21,13 @@ public class RequestDialog {
     private void initDialog(Dialog dialog){
 
     }
+    @SuppressWarnings("deprecation")
     public void show(){
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         reqDialog= new Dialog(context, android.R.style.Theme_Black);
         reqDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         reqDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-        reqDialog.setContentView(inflater.inflate(R.layout.layout_main, null));
+        reqDialog.setContentView(inflater.inflate(R.layout.layout_req_detail, null));
         initDialog(reqDialog);
         reqDialog.show();
     }
